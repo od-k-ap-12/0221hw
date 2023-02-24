dea485577cc00ff950df271d195dd402de57d848
Oleksandra <predictormagnolia@gmail.com>
2023-02-24 06:36:58 +0200

    Add project files.

---
+++ 0221hw/Backpack.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0221hw
+{
+    public delegate void ObjEv();
+    class Backpack
+    {
+        public string color { get; set; }
+        public string brand { get; set; }
+        public string fabric { get; set; }
+        public int weight { get; set; }
+        public static int capacity { get; set; }
+        static Object[] objs;
+        static int objCount=0;
+        static int objMaxCount = 10;
+        public event ObjEv ev;
+        public Backpack(string _color, string _brand, string _fabric, int _weight, int _capacity)
+        {
+            color = _color;
+            brand = _brand;
+            fabric = _fabric;
+            weight = _weight;
+            capacity = _capacity;
+            objs = new Object[objMaxCount];
+        }
+        public ObjEv AddObject = delegate ()
+          {
+              if (objCount == objMaxCount)
+              {
+                  throw new Exception("Can't add no more");
+              }
+              else
+              {
+                  objs[objCount].Input();
+                  if (objs[objCount].capacitytake > capacity)
+                  {
+                      throw new Exception("Not enough capacity to add this obj");
+                  }
+                  else
+                  {
+                      objCount++;
+                      capacity -= objs[objCount].capacitytake;
+                      Console.WriteLine("Object is successfully added");
+                  }
+              }
+          };
+        public void AddObjCall()
+        {
+            ev?.Invoke();
+        }
+        public void Print()
+        {
+            Console.WriteLine("Color: " + color + "\nBrand: " + brand+"\nFabric: "+fabric+"\nWeight: "+weight+"\nCapacity: "+capacity+"\nObjs: "+string.Join(" ",objs));
+        }
+        public override string ToString()
+        {
+            return "Color: " + color + "\nBrand: " + brand + "\nFabric: " + fabric + "\nWeight: " + weight + "\nCapacity: " + capacity + "\nObjs: " + string.Join(" ", objs);
+        }
+        public void Input()
+        {
+            Console.WriteLine("Enter color: ");
+            color = Console.ReadLine();
+            Console.WriteLine("Enter brand: ");
+            brand = Console.ReadLine();
+            Console.WriteLine("Enter fabric: ");
+            fabric = Console.ReadLine();
+            Console.WriteLine("Enter weight: ");
+            weight = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter capacity: ");
+            capacity = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
+    struct Object
+    {
+        public string name { get; set; }
+        public int capacitytake { get; set; }
+        public Object(string _name, int _capacitytake)
+        {
+            name = _name;
+            capacitytake = _capacitytake;
+        }
+        public void Print()
+        {
+            Console.WriteLine("Name: " + name + "\nFree space taken: " + capacitytake);
+        }
+        public override string ToString()
+        {
+            return "Name: " + name + "\nFree space taken: " + capacitytake;
+        }
+        public void Input()
+        {
+            Console.WriteLine("Enter name: ");
+            name = Console.ReadLine();
+            Console.WriteLine("How much free space it takes: ");
+            capacitytake = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+
---
+++ 0221hw/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0221hw
+{
+    delegate string RGBRainbow(string color);
+    delegate int ArrayInt(int[] arr);
+    delegate void ArrayVoid(int[] arr);
+    delegate bool StringCheck(string str,string word);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region 1
+            RGBRainbow getRGB = delegate (string color)
+            {
+                string[] colors = new string[] { "Red", "Orange", "Yellow", "Green", "Light Blue", "Blue", "Purple" };
+                string[] RGBs = new string[] { "255, 0 , 0", "255, 127, 0", "255, 255, 0", "0, 255, 0", "0, 0, 255", "75, 0, 130", "148, 0, 211" };
+                for(int i=0;i<colors.Length;i++)
+                {
+                    if (colors[i] == color)
+                    {
+                        return RGBs[i];
+                    }
+                }
+                return "Not found";
+            };
+            Console.WriteLine(getRGB("Red"));
+            #endregion
+
+            #region 2
+            Backpack bp = new Backpack("color", "brand", "fabric", 2000, 3000);
+            bp.ev += new ObjEv(bp.AddObject);
+            bp.AddObjCall();
+
+            #endregion
+
+            #region 3
+            int[] arr = new int[] { -8, 2, 7, 14, 56, -6, 49, 8, 14, -10 };
+            ArrayInt MultipleOf7 = (int[] array) =>
+            {
+                int count = 0;
+                foreach (int i in array)
+                {
+                    if (i % 7 != 0)
+                    {
+                        count++;
+                    }
+                }
+                return count;
+            };
+            Console.WriteLine(MultipleOf7(arr));
+
+            #endregion
+
+            #region 4
+            ArrayInt PositiveNum = (int[] array) =>
+            {
+                int count = 0;
+                foreach (int i in array)
+                {
+                    if (i > 0)
+                    {
+                        count++;
+                    }
+                }
+                return count;
+            };
+            Console.WriteLine(PositiveNum(arr));
+            #endregion
+
+            #region 5
+            ArrayVoid UniqueNegativeNum = (int[] array) =>
+            {
+                Array.Sort(array);
+                for(int i = 0; i < array.Length-1; i++)
+                {
+                    if (array[i + 1] != array[i] && array[i] < 0)
+                    {
+                        Console.WriteLine(array[i]);
+                    }
+                }
+            };
+            UniqueNegativeNum(arr);
+            #endregion
+
+            #region 6
+            StringCheck ifContains = (string str, string word) => str.Contains(word);
+            Console.WriteLine(ifContains("Sample string", "Sample"));
+            #endregion
+
+
+        }
+    }
+}
